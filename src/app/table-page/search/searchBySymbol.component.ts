0c3c584c7b72ec0df1c98fb45e2c576e50515676
Volodymyr <volodymyr.derunov@loopme.com>
2020-05-07 16:17:44 +0300

    refactor: searchBySymbol component

--- src/app/table-page/search/searchBySymbol.component.ts
+++ src/app/table-page/search/searchBySymbol.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 import { RequestStateService } from '../../shared/request-state.service';
 import { ISearchBySymbol } from './interfaces';
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 
 @Component({
@@ -9,19 +10,28 @@ import { ISearchBySymbol } from './interfaces';
   templateUrl: './searchBySymbol.component.html',
   styleUrls: ['./searchBySymbol.component.scss']
 })
-export class SearchBySymbolComponent implements ISearchBySymbol {
+export class SearchBySymbolComponent implements OnInit, ISearchBySymbol {
+
+  private stream$: Subject<string> = new Subject<string>();
 
   constructor(private requestStateService: RequestStateService) {
   }
 
-  public searchValueEvent($event: any): void {
-    const value: string = $event.target.value;
+  ngOnInit(): void {
+    this.stream$.pipe(
+      debounceTime(1000),
+      distinctUntilChanged()
+    ).subscribe(value => {
+      if (value) {
+        this.requestStateService.setState({symbols: value});
+      } else {
+        this.requestStateService.setState({symbols: false});
+      }
+    });
+  }
 
-    if (value) {
-      this.requestStateService.setState({symbols: value});
-    } else {
-      this.requestStateService.setState({symbols: false});
-    }
+  public searchValueEvent($event: any) {
+    this.stream$.next($event.target.value);
   }
 }
 
